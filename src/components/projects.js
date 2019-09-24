26c6983b6f0fff587c4fa5b420df3bb5d97feb8f
annazetterstrom <anna.wiktoria@me.com>
2019-09-24 09:24:22 +0200

    img

--- src/components/projects.js
+++ src/components/projects.js
@@ -4,19 +4,19 @@ import Project from "./project";
 let projectArray = [
   {
     project: "Webshop", 
-    picture: "/static/webshop.png", 
+    picture: "/webshop.png", 
     excerpt: "This is a webshop we created in our Javascript course", 
     link: "https://annazetterstrom.github.io/webshop/"
   },
   {
     project: "Frontend Project", 
-    picture: "/static/frontend.png", 
+    picture: "/frontend.png", 
     excerpt: "This is a website with tutorials in HTML, CSS, Javascript and Adobe XD", 
     link: "https://annazetterstrom.github.io/fend-tutorials/"
   },
   {
     project: "Html & CSS",
-    picture: "/static/b3.jpg",
+    picture: "/b3.jpg",
     excerpt: "This is....",
     link: "https://annazetterstrom.github.io/project/"
   }
