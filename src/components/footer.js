6bad6ecb32558fc51136d1a8ce28757559dc0e46
annazetterstrom <anna.wiktoria@me.com>
2019-09-24 13:02:47 +0200

    static

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,19 +3,19 @@ import React from "react";
 export default () => (
     <footer>
        <a href="mailto:anna.wiktoria@me.com">
-       <img src="email.svg"></img> 
+       <img src="/email.svg"></img> 
         </a>
 
         <a href="tel:070-0373004">
-        <img src="phone.svg"></img> 
+        <img src="/phone.svg"></img> 
         </a>
 
         <a  href="https://github.com/annazetterstrom">
-        <img src="github.svg"></img> 
+        <img src="/github.svg"></img> 
         </a>
 
         <a  href="https://www.linkedin.com/in/anna-zetterstr%C3%B6m-585259b0">
-        <img src="linkedin.svg"></img> 
+        <img src="/linkedin.svg"></img> 
         </a>
     </footer>
 )
