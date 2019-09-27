ca36f2c4ddbd65116814f5bd0af2cc8dcd27547e
annazetterstrom <anna.wiktoria@me.com>
2019-09-27 14:21:07 +0200

    404

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,6 +6,7 @@ const NotFoundPage = () => (
   <Layout>
     <h1>NOT FOUND</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <img src="/404.png" alt="404" />
   </Layout>
 )
 
